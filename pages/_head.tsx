6e584c21773b4bd49214a10252a88e1a765b4bd4
Xoje Laptop <dawxoje@gmail.com>
2023-02-07 16:44:32 +0100

    add head

--- pages/_head.tsx
+++ pages/_head.tsx
@@ -1,16 +1,18 @@
-const Head = () => {
+import Head from 'next/head'
+
+const HeadCustom = () => {
   const headData = {
     title: 'Jose Vilches Porfolio',
     description: 'Este porfolio mola'
   }
   return (
-    <>
+    <Head>
       <title>{headData.title}</title>
       <meta name='description' content={headData.description} />
 
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <link rel='icon' href='/favicon.ico' />
-    </>
+    </Head>
   )
 }
-export default Head
+export default HeadCustom
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { Experimental_CssVarsProvider as CssVarsProvider, getInitColorSchemeScript } from '@mui/material/styles'
 import { theme } from '@/utils/themes'
 import Porfolio from '@/components/porfolio'
+import HeadCustom from '@/pages/_head'
 
 const Home = () =>
   <CssVarsProvider theme={theme}>
     {getInitColorSchemeScript()}
+    <HeadCustom />
     <Porfolio />
   </CssVarsProvider>
 
