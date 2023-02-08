c2be0455ef1939f01d4a81b85a8eba8df3710713
Xoje Laptop <dawxoje@gmail.com>
2023-02-08 09:22:01 +0100

    ready to vercel

--- lib/porfolioData.ts
+++ lib/porfolioData.ts
@@ -1,7 +1,6 @@
 export const linkedinUrl = 'https://www.linkedin.com/in/jose-vilches-sanchez/'
 
 const myMail = 'vilchessanchezjose@gmail.com'
-const subjectMail = 'Contratación'
-const messageMail = 'Buenos dias Jose, he visto tu curriculum y estamos interesados en que te incorpores lo antes posible en el puesto de ...'
-// const messageMail = 'algo'
-export const mailtoUrl = `mailto:${myMail}?subject=${subjectMail}&body=${messageMail}`
+// const subjectMail = 'Contratación'
+// const messageMail = 'Buenos dias Jose, he visto tu curriculum y estamos interesados en que te incorpores lo antes posible en el puesto de ...'
+export const mailtoUrl = `mailto:${myMail}`// ?subject=${subjectMail}&body=${messageMail}`
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import { Experimental_CssVarsProvider as CssVarsProvider, getInitColorSchemeScript } from '@mui/material/styles'
 import { theme } from '@/utils/themes'
 import Porfolio from '@/components/porfolio'
-import HeadCustom from '@/pages/_head'
+import Head from 'next/head'
 
+const headData = {
+  title: 'Jose Vilches Porfolio',
+  description: 'Este porfolio mola'
+}
 const Home = () =>
   <CssVarsProvider theme={theme}>
     {getInitColorSchemeScript()}
-    <HeadCustom />
+    <Head>
+      <title>{headData.title}</title>
+      <meta name='description' content={headData.description} />
+
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
     <Porfolio />
   </CssVarsProvider>
 
