70cf4a5e84bf02641a306a66d3e728bd1354b145
Xoje Laptop <dawxoje@gmail.com>
2023-02-09 08:05:38 +0100

    feat(project): add todo state

--- components/molecules/Project.tsx
+++ components/molecules/Project.tsx
@@ -1,42 +1,67 @@
 'use client'
 import { Project as ProjectModel } from '@/lib/models/Project'
-import { Button, Card, CardActions, CardContent, Paper, Skeleton, Stack, SxProps, Theme, Typography } from '@mui/material'
+import { Button, Card, CardActions, CardContent, Skeleton, SxProps, Theme, Typography } from '@mui/material'
 import styles from '../../styles/Project.module.scss'
 import useTranslation from 'next-translate/useTranslation'
 import Image from 'next/image'
 import ParagraphShowMore from '@/components/atoms/ParagraphShowMore'
 /* import { BsFillFileCodeFill } from 'react-icons/bs'
 import { MdWeb } from 'react-icons/md' */
-const ProjectSkeleton = () => {
-  const title = 'algo'
-  const sxInline: SxProps<Theme> = {
-    display: ''
-  }
-  return (
-    <>
-      <Paper sx={{ height: 400 }}>
-        <Stack spacing={1}>
-
-          {/* For other variants, adjust the size with `width` and `height` */}
-          <Skeleton variant='rectangular' height={140} />
-          <Typography>{title}</Typography>
-          <Skeleton variant='text' sx={{ fontSize: '1rem' }} width={200} />
-          <Skeleton variant='text' sx={{ fontSize: '1rem' }} width={200} />
-          <Skeleton variant='text' sx={{ fontSize: '1rem', ...sxInline }} width={150} />
-          <Skeleton variant='circular' sx={{ ...sxInline }} width={40} height={40} />
-
-          <Skeleton variant='rounded' sx={{ ...sxInline }} width={90} height={50} />
-          <Skeleton variant='rounded' sx={{ ...sxInline }} width={90} height={50} />
-        </Stack>
-      </Paper>
-    </>
-  )
-}
 
 export default function Project ({ project }: { project: ProjectModel }) {
   const { title, description, image, state } = project
   const { t } = useTranslation('projects')
+
+  const titleTranslated = t(title)
   // const [isZoom, setIsZoom] = useState(false)
+  const ButtonSkeleton = () => <Skeleton
+    variant='rounded'
+    className={styles.project__button}
+    height={50}
+                               />
+  const ProjectSkeleton = () => {
+    const sxInline: SxProps<Theme> = {}
+    return (
+      <>
+        {/* <Paper sx={{ height: 400 }}>
+          <Stack spacing={1}>
+
+            <Skeleton variant='rectangular' height={140} />
+            <Typography variant='h3'>{title}</Typography>
+            <Skeleton variant='text' sx={{ fontSize: '2rem' }} width={200} />
+            <Skeleton variant='text' sx={{ fontSize: '1rem' }} width={200} />
+            <div className='lastLine'>
+              <Skeleton variant='text' sx={{ fontSize: '1rem', ...sxInline }} width={150} />
+              <Skeleton variant='circular' sx={{ ...sxInline }} width={40} height={40} />
+            </div>
+            <div className='group_button'>
+              <Skeleton variant='rounded' sx={{ ...sxInline }} width={90} height={50} />
+              <Skeleton variant='rounded' sx={{ ...sxInline }} width={90} height={50} />
+            </div>
+          </Stack>
+        </Paper> */}
+        <Card className={`${styles.project} `}>
+          <Skeleton variant='rectangular' height={140} className={styles.project__image} />
+          <CardContent>
+            <Skeleton
+              variant='text' sx={{ fontSize: '2.5rem' }} width={130}
+              className={styles.project__title}
+            />
+            <Skeleton variant='text' sx={{ fontSize: '1rem' }} width={200} />
+            <Skeleton variant='text' sx={{ fontSize: '1rem' }} width={200} />
+            <Skeleton variant='text' sx={{ fontSize: '1rem', ...sxInline }} width={150} />
+            <Skeleton variant='circular' sx={{ ...sxInline }} width={40} height={40} />
+
+          </CardContent>
+          <CardActions className={styles.project__group_buttons}>
+            <ButtonSkeleton />
+            <ButtonSkeleton />
+          </CardActions>
+        </Card>
+      </>
+    )
+  }
+
   const ProjectComplete = () => {
     return (
       <Card
@@ -44,13 +69,6 @@ export default function Project ({ project }: { project: ProjectModel }) {
       >
         {/* TODO: Al cambiar de elemento para la imagen se ha descentrado */}
         {/* TODO: Y en concreto el primer proyecto falla en el lighthouse de brave */}
-        {/* <Zoom in={isZoom} timeout={1000}>
-          <Image
-            src={image.src} alt={t(image.alt)} width={468}
-            height={1000}// 140h?
-            className={styles.project__image}
-          />
-        </Zoom> */}
         <div className={styles.wrapper__image}>
           <Image
             src={image.src} alt={t(image.alt)} width={468}
@@ -68,7 +86,7 @@ export default function Project ({ project }: { project: ProjectModel }) {
           /> */}
         <CardContent>
           <Typography gutterBottom variant='h5' component='div' className={styles.project__title}>
-            {t(title)}
+            {titleTranslated}
 
           </Typography>
           <ParagraphShowMore content={t(description)} maxLength={100} />
